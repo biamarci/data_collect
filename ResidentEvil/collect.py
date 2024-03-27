62abb518c6cba0a07a7c3414a28eeedb28f80c59
biamarci <beatrizmarci@ufrj.br>
2024-03-26 23:16:17 -0300

    Coleta de dados do Resident Evil Database - Código completo

--- ResidentEvil/collect.py
+++ ResidentEvil/collect.py
@@ -1,11 +1,10 @@
-#%%
-print("Foi?")
 # %%
 import requests 
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import pandas as pd
 
 #%%
-url = "https://www.residentevildatabase.com/personagens/alex-wesker/"
-
 cookies = {
     '_gid': 'GA1.2.1514768992.1711414075',
     '_ga_DJLCSW50SC': 'GS1.1.1711414068.1.1.1711414088.40.0.0',
@@ -31,11 +30,72 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
 }
 #%%
-resp = requests.get(url, cookies=cookies, headers=headers)
+
+def get_links():
+    url = 'https://www.residentevildatabase.com/personagens/'
+    resp = requests.get(url, headers=headers, cookies=cookies)
+    soup_personagens = BeautifulSoup(resp.text)
+    ancoras = (soup_personagens.find('div', class_="td-page-content")
+                            .find_all('a'))
+
+    links = [i['href'] for i in ancoras]
+    return links 
+
+def get_content(url):
+        resp = requests.get(url, cookies=cookies, headers=headers)
+        return resp
+
+def get_info(soup):
+    div_page = soup.find('div',class_="td-page-content")
+    paragrafo = div_page.find_all('p')[1]
+    linhas = paragrafo.find_all('em')
+    data = {}
+    for i in linhas :
+        chave, valor, *_ = i.text.split(':')
+        chave= chave.strip(' ')
+        valor = valor.strip(' ')
+        data[chave]=valor
+    return data 
+
+def get_aparicoes(soup):
+    lis = (soup.find('div', class_="td-page-content")
+        .find('h4')
+        .find_next()
+        .find_all('li'))
+    aparicoes = [i.text for i in lis]
+    return aparicoes
+#%%
+# Fazer a conexão
+url = "https://www.residentevildatabase.com/personagens/ada-wong/"
+
+def get_personagem(url):
+    resp = get_content(url)
+
+    # Verificar se deu certo e buscar infos
+    if resp.status_code !=200:
+        print ("Problema na extração")
+    else:
+        soup = BeautifulSoup(resp.text)
+        data = get_info(soup)
+        data['Aparicoes'] = get_aparicoes(soup)
+    return data
+
 
 # %%
-resp.status_code
-# %%
-## 54 min - aula 1
 
+links = get_links()
+data = []
+
+for i in tqdm(links):
+     print(d)
+     d=get_personagem(i)
+     d['link']=i
+     nome = i.strip('/').split('/')[-1].replace('-',' ').title()
+     d["Nome"]=nome
+     data.append(d)
+
+
+# %%
+df = pd.DataFrame(data) 
+df.to_csv("dados_re", index=False, sep=';')
 # %%
